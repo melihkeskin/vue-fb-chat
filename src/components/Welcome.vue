926e050532df945ee33ffbfa8509126b64cd3307
Melih Keskin <melihkeskinn@gmail.com>
2020-06-02 11:56:33 +0200

    first commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">
+          welcome
+        </h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your name:</label>
+          <input type="text" name="name" v-model="name" />
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
+          <button class="btn teal">
+            Enter Chat
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      name: null,
+      feedback: null
+    };
+  },
+  methods: {
+    enterChat() {
+      if (this.name) {
+        this.feedback = null;
+        this.$router.push({name:'Chat'})
+      } else {
+        this.feedback = "You must enter a name to join";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.welcome {
+  max-width: 400px;
+  margin-top: 100px;
+}
+
+.welcome h2 {
+  font-size: 3em;
+}
+
+.welcome button {
+  margin: 30px auto;
+}
+ 
+</style>
